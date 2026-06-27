917e9bb3d63e954379f99e6cd1887f62f6af30d4
unknown <para8e11um1@gmail.com>
2026-06-28 04:22:03 +0600

    feat: implement task 4

---
+++ practice2026/task04/ISpaceship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();
+        void Rotate(int angle);
+        void Fire();
+        int Speed { get; }
+        int FirePower { get; }
+    }
+}
---
+++ practice2026/task04tests/SpaceshipTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using task04;
+public class SpaceshipTests
+{
+    [Fact]
+    public void Test1()
+    {
+        ISpaceship cruiser = new Cruiser();
+        Assert.Equal(50,cruiser.Speed);
+        Assert.Equal(100,cruiser.FirePower);
+    }
+    [Fact]
+    public void Test2()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        Assert.True(fighter.Speed > cruiser.Speed);
+    }
+    [Fact]
+    public void Test3()
+    {
+        ISpaceship fighter = new Fighter();
+        Assert.Equal(100, fighter.Speed);
+        Assert.Equal(50, fighter.FirePower);
+    }
+}
