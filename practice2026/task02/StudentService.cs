e94c9dec5aae4fe6aff91f242f45dd0a2ed61028
unknown <para8e11um1@gmail.com>
2026-06-28 00:37:58 +0600

    feat: implement task 2 solution

---
+++ practice2026/task02/StudentService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task02
+{
+    public class StudentService
+    {
+        List<Student> students;
+
+        public StudentService(List<Student> Students)
+        {
+            students = Students;
+
+        }
+
+        public IEnumerable<Student> GetStudentsByFaculty(string FacultyName)
+        {
+            return from student in students
+                   where student.Faculty == FacultyName
+                   select student;
+        }
+
+        public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverage)
+        {
+            return from student in students
+                   where student.Grades.Average() > minAverage
+                   select student;
+        }
+
+        public IEnumerable<Student> GetStudentsOrderedByName()
+        {
+            return from student in students
+                   orderby student.Name
+                   select student;
+        }
+        public IEnumerable<IGrouping<string,Student>> GetStudentsGroupedByFaculty() {
+            return from student in students
+                   group student by student.Faculty
+                   into studentGroup
+                   select studentGroup;
+        }
+        public string GetHighestAverageFaculty()
+        {
+            return (from student in students
+                    group student by student.Faculty into studentGroup
+                    orderby (from student in studentGroup
+                             select student.Grades.Average()).ToArray().Average() descending
+                    select studentGroup).ToArray()[0].Key;
+        }
+    }
+}
---
+++ practice2026/task02tests/StudentServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+using System.Collections;
+using System.Linq;
+using task02;
+
+public class StudentServiceTests
+{
+    private List<Student> _testStudents;
+    private StudentService _studentService;
+    public StudentServiceTests()
+    {
+        _testStudents = new List<Student>
+        {
+            new Student { Name = "Иван", Faculty = "ФИТ", Grades = new List<int> {5, 4, 5}},
+            new Student { Name = "Анна", Faculty = "ФИТ", Grades = new List<int> {3,4,3}},
+            new Student { Name = "Петр", Faculty = "Экономика", Grades = new List<int> {5,5,5}}
+        };
+        _studentService = new StudentService(_testStudents);
+    }
+    [Fact]
+    public void Test1()
+    {
+        var result = _studentService.GetStudentsByFaculty("ФИТ").ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Faculty == "ФИТ"));
+    }
+    [Fact]
+    public void Test2()
+    {
+        var result = _studentService.GetHighestAverageFaculty();
+        Assert.Equal("Экономика", result);
+    }
+    [Fact]
+    public void Test3()
+    {
+        var result = _studentService.GetStudentsWithMinAverageGrade(4.5).ToList();
+        Assert.Equal(2, result.Count);
+        Assert.Contains(result, s => s.Name == "Петр");
+        Assert.Contains(result, s => s.Name == "Иван");
+    }
+    [Fact]
+    public void Test4()
+    {
+        var result = _studentService.GetStudentsOrderedByName().ToList();
+        Assert.Equal(result[0].Name, "Анна");
+        Assert.Equal(result[1].Name, "Иван");
+        Assert.Equal(result[2].Name, "Петр");
+    }
+    [Fact]
+    public void Test5()
+    {
+        var result = _studentService.GetStudentsGroupedByFaculty().ToList();
+        Assert.Equal(2, result.Count());
+        var fitGroup = result.FirstOrDefault(g => g.Key == "ФИТ");
+        Assert.NotNull(fitGroup);
+        Assert.Equal(2, fitGroup.Count());
+        var ecoGroup = result.FirstOrDefault(g => g.Key == "Экономика");
+        Assert.NotNull(ecoGroup);
+        Assert.Single(ecoGroup);
+    }
+}
