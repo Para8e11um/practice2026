f4cbc4025143c27e9816f48d168f72ab6f263528
unknown <para8e11um1@gmail.com>
2026-07-12 16:57:11 +0600

    Add Task 8 and CI

---
+++ practice2026/CommandRunner/Program.cs
@@ -0,0 +1,49 @@
+﻿using CommandLib;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+namespace CommandRunner
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string targetDir = AppDomain.CurrentDomain.BaseDirectory;
+            string dllPath = Path.GetFullPath(Path.Combine(targetDir, "..", "..", "..", "..", "FileSystemCommands", "bin", "Debug", "net9.0", "FileSystemCommands.dll"));
+            if (!File.Exists(dllPath))
+            {
+                dllPath = Path.Combine(targetDir, "FileSystemCommands.dll");
+                if (!File.Exists(dllPath))
+                {
+                    Console.WriteLine($"Сборка не найдена в: " + dllPath);
+                    return;
+                }
+            }
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(dllPath);
+
+                var commandTypes = assembly.GetTypes().Where(t => typeof(ICommand).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+                foreach (var commandType in commandTypes)
+                {
+                    Console.WriteLine("Инициализация и выполнение " + commandType.Name);
+                    ICommand command = null;
+                    if (commandType.Name == "DirectorySizeCommand")
+                    {
+                        command = (ICommand)Activator.CreateInstance(commandType, targetDir);
+                    }
+                    else if (commandType.Name == "FindFilesCommand")
+                    {
+                        command = (ICommand)(Activator.CreateInstance(commandType, targetDir, "*.dll"));
+                    }
+                    command?.Execute();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка:" + ex.Message);
+            }
+        }
+    }
+}
---
+++ practice2026/task08tests/FileSystemCommandsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Xunit;
+using FileSystemCommands;
+public class FileSystemCommandsTests
+{
+    public class FileSystemCommands
+    {
+        [Fact]
+        public void DirectorySizeCommand_ShouldCalculateSize()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDirSize");
+            Directory.CreateDirectory(testDir);
+
+            var test1Path = Path.Combine(testDir, "test1.txt");
+            var test2Path = Path.Combine(testDir, "test2.txt");
+            File.WriteAllText(test1Path, "Hello");
+            File.WriteAllText(test2Path, "World");
+
+            long expectedSize = new FileInfo(test1Path).Length + new FileInfo(test2Path).Length;
+            var command = new DirectorySizeCommand(testDir);
+            command.Execute();
+            Assert.Equal(expectedSize, command.Size);
+            Directory.Delete(testDir, true);
+        }
+        [Fact]
+        public void FindFilesCommand_ShouldFindMatchingFiles()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDirFind");
+            Directory.CreateDirectory(testDir);
+            var targetFile = Path.Combine(testDir, "file1.txt");
+            var ignoreFile = Path.Combine(testDir, "file2.log");
+            File.WriteAllText(targetFile, "Text content");
+            File.WriteAllText(targetFile, "Log content");
+            var command = new FindFilesCommand(testDir, "*.txt");
+            command.Execute();
+            Assert.Single(command.FoundFiles);
+            Assert.Contains(targetFile, command.FoundFiles);
+            Directory.Delete(testDir, true);
+        }
+    }
+}
